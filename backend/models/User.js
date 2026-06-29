e8fbd0946d1233e0ec763d4f4ecfbfbcc2f40dc4
YAMOVIES <ya753121988@gmail.com>
2026-06-29 22:36:11 +0600

    Delete backend/models directory

--- backend/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const UserSchema = new mongoose.Schema({
-    fullName: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    balance: { type: Number, default: 0 },
-    role: { type: String, default: 'user' }
-});
-module.exports = mongoose.model('User', UserSchema);
