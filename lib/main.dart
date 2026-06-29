df103b08973bf9f46646f23c854f6031969e0600
YAMOVIES <ya753121988@gmail.com>
2026-06-29 21:40:34 +0600

    Update lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,2 @@
 import 'package:flutter/material.dart';
-void main()=>runApp(MaterialApp(home:Scaffold(appBar:AppBar(title:Text('FlixBixs')),body:Center(child:Text('https://flixboxsmovies.blogspot.com/')))));
+void main()=>runApp(MaterialApp(home:Scaffold(body:Center(child:Text('Ready')))));
