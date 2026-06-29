dc3c249effe123e75f0e22db7e768ce886886673
YAMOVIES <ya753121988@gmail.com>
2026-06-29 22:58:47 +0600

    Delete lib directory

--- lib/main.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-void main()=>runApp(MaterialApp(home:Scaffold(appBar:AppBar(title:Text('FlixBoxs')),body:WebViewWidget(controller:WebViewController()..loadRequest(Uri.parse('https://flixboxsmovies.blogspot.com/'))))));
