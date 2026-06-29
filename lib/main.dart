575fae380f4e0cf0487f0781abd6e3187d996cff
YAMOVIES <ya753121988@gmail.com>
2026-06-29 21:16:12 +0600

    Auto Setup: lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,2 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('APP_NAME_PLACEHOLDER'), // এখানে অ্যাপের নাম বসবে
-          backgroundColor: Colors.blue,
-        ),
-        body: WebViewWidget(
-          controller: WebViewController()
-            ..setJavaScriptMode(JavaScriptMode.unrestricted)
-            ..loadRequest(Uri.parse('URL_PLACEHOLDER')), // এখানে ওয়েবসাইট লিঙ্ক বসবে
-        ),
-      ),
-    );
-  }
-}
+void main()=>runApp(MaterialApp(home:Scaffold(body:Center(child:Text('System Ready')))));
